70a5697e90cd6b4e3841546386587be0226f085f
Nico Felbinger <nico@felbinger.eu>
2025-12-02 19:18:18 +0100

    tests/bgp-extended-nexthop: add routes

--- tests/bgp-extended-nexthop/default.nix
+++ tests/bgp-extended-nexthop/default.nix
@@ -17,6 +17,9 @@
       services.frr = {
         bgpd.enable = true;
         config = ''
+          ip route 198.51.100.0/25 reject
+          ipv6 route 2001:db8:beef::/48 reject
+
           router bgp 64496
             no bgp ebgp-requires-policy
             no bgp default ipv4-unicast
@@ -27,10 +30,12 @@
             neighbor fe80::2 interface eth1
 
             address-family ipv4 unicast
+              network 198.51.100.0/25
               neighbor fe80::2 activate
             exit-address-family
 
             address-family ipv6 unicast
+              network 2001:db8:beef::/48
               neighbor fe80::2 activate
             exit-address-family
         '';
@@ -68,11 +73,17 @@
           }
 
           protocol static static4 {
+            vrf "vrf0";
             ipv4;
+
+            route 198.51.100.128/25 unreachable;
           }
 
           protocol static static6 {
+            vrf "vrf0";
             ipv6;
+
+            route 2001:db8:c0de::/48 unreachable;
           }
 
           protocol bgp a {
@@ -105,8 +116,15 @@
     a.wait_for_unit("frr.service")
     b.wait_for_unit("bird.service")
 
-    a.wait_until_succeeds("vtysh -c 'show bgp ipv4 summary' | grep 'fe80::2.*0\\s*0\\s*N/A'")
-    a.wait_until_succeeds("vtysh -c 'show bgp ipv6 summary' | grep 'fe80::2.*0\\s*0\\s*N/A'")
-    b.wait_until_succeeds("birdc show protocols | grep 'a.*Established'")
+    with subtest("ensure bgp sessions are established"):
+      a.wait_until_succeeds("vtysh -c 'show bgp ipv4 summary' | grep 'fe80::2.*1\\s*2\\s*N/A'")
+      a.wait_until_succeeds("vtysh -c 'show bgp ipv6 summary' | grep 'fe80::2.*1\\s*2\\s*N/A'")
+      b.wait_until_succeeds("birdc show protocols | grep 'a.*Established'")
+
+    with subtest("ensure routes have been installed"):
+      a.succeed("ip route show | grep 198.51.100.128/25")
+      b.succeed("ip route show | grep 198.51.100.0/25")
+      a.succeed("ip -6 route show | grep 2001:db8:c0de::/48")
+      b.succeed("ip -6 route show | grep 2001:db8:beef::/48")
   '';
 }
